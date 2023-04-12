8a565456ad68bfa2b09e2515425322c7d5587b67
V3ss3l <grishav3ss3l@gmail.com>
2023-04-12 22:22:20 +0400

    Search field is ready (visual)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PortalComponent } from './portal/portal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatInputModule} from "@angular/material/input";
+import {MatExpansionModule} from "@angular/material/expansion";
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8188', // поставить свой порт для кейклока
+        realm: 'daily-quotes', // поставить свой realm
+        clientId: 'frontend'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KeycloakAngularModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatInputModule,
+    MatExpansionModule,
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    }*/
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
